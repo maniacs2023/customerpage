3a1c997ed1ba3bfe6e9806a14b1ac6e5a7a28456
Sounak Guha <65349891+DarkKingStar@users.noreply.github.com>
2022-11-08 19:47:10 +0530

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,14 +19,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 const router = useRouter()
 const cookie = parseCookies()
 useEffect(()=>{
-  if(cookie)
-  {
     if(cookie.theme == 'dark'){
       document.documentElement.style.setProperty('--back-gd-color', '#1f2522');
       document.documentElement.style.setProperty('--text-color', '#cdcdcd');
       document.documentElement.style.setProperty('--second-bg-color', '#222220');
     }
-  }
   else{
     document.documentElement.style.setProperty('--back-gd-color', '#fff');
     document.documentElement.style.setProperty('--text-color', '#212529');
