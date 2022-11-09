a7cf855a1cb4e51a3b6d7196cffb9655a678cf79
Sounak Guha <65349891+DarkKingStar@users.noreply.github.com>
2022-11-09 13:37:45 +0530

    initial

--- component/homecomponent/json/offerUrl.js
+++ component/homecomponent/json/offerUrl.js
@@ -1,18 +1,18 @@
 module.exports = [
     {"key":1, 
-     "url": require("../../../public/img/offer1.webp").default.src,
+     "url": require("../../../public/img/offer/offer1.webp").default.src,
     "code": "FIRST20"
     },
     {"key":2, 
-     "url": require("../../../public/img/offer2.webp").default.src,
+     "url": require("../../../public/img/offer/offer2.webp").default.src,
     "code": "COLOUR15"
     },
     {"key":3, 
-     "url": require("../../../public/img/offer3.webp").default.src,
+     "url": require("../../../public/img/offer/offer3.webp").default.src,
     "code": "DRIVE25"
     },
     {"key":4,
-     "url": require("../../../public/img/offer4.webp").default.src,
+     "url": require("../../../public/img/offer/offer4.webp").default.src,
     "code": "COOK10"
     }
 ]
